a53586f85ee805301739e3ab5b4dc81736213a44
honnu-18 <honnu-shree@outlook.com>
2026-03-31 19:20:25 +0530

    prepare for deploymentttttt

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.API_BASE;
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE;
 export const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
